982eff184b44f84ee0cca9259e6335e331dabaae
feat-mrqxzz777 <gaelooomrz07@gmail.com>
2026-08-11 21:30:46 -0600

    Se agrega la estructura limpia del proyecto KiiwikPOS de NetBeans

---
+++ KiiwikPOS/src/main/java/com/kiiwik/pos/KiiwikPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kiiwik.pos;
+
+/**
+ *
+ * @author gaelooo77
+ */
+public class KiiwikPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
